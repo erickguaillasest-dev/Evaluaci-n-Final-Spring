57a61d479e362bd5ac139d2925a41b14eaf0440f
Erick Guaillas <erickguaillas1987@gmail.com>
2026-08-18 13:32:27 -0500

    condominio

--- src/main/java/com/facilpago/dto/CondominioRequestDTO.java
+++ src/main/java/com/facilpago/dto/CondominioRequestDTO.java
@@ -11,8 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CondominioRequestDTO {
-	
-	@JsonProperty("cedulaCondomino")
+
+    @JsonProperty("cedulaCondomino")
     @JsonAlias({"cedula", "cedulaCondominio"}) 
     private String cedulaCondomino;
     private String nombreCondomino;
@@ -21,49 +21,60 @@ public class CondominioRequestDTO {
     private String telefonoCondomino;
     private String numeroBloque;
     private String numeroDepartamento;
-	public String getCedulaCondomino() {
-		return cedulaCondomino;
-	}
-	public void setCedulaCondomino(String cedulaCondomino) {
-		this.cedulaCondomino = cedulaCondomino;
-	}
-	public String getNombreCondomino() {
-		return nombreCondomino;
-	}
-	public void setNombreCondomino(String nombreCondomino) {
-		this.nombreCondomino = nombreCondomino;
-	}
-	public String getApellidoCondomino() {
-		return apellidoCondomino;
-	}
-	public void setApellidoCondomino(String apellidoCondomino) {
-		this.apellidoCondomino = apellidoCondomino;
-	}
-	public String getCelularCondomino() {
-		return celularCondomino;
-	}
-	public void setCelularCondomino(String celularCondomino) {
-		this.celularCondomino = celularCondomino;
-	}
-	public String getTelefonoCondomino() {
-		return telefonoCondomino;
-	}
-	public void setTelefonoCondomino(String telefonoCondomino) {
-		this.telefonoCondomino = telefonoCondomino;
-	}
-	public String getNumeroBloque() {
-		return numeroBloque;
-	}
-	public void setNumeroBloque(String numeroBloque) {
-		this.numeroBloque = numeroBloque;
-	}
-	public String getNumeroDepartamento() {
-		return numeroDepartamento;
-	}
-	public void setNumeroDepartamento(String numeroDepartamento) {
-		this.numeroDepartamento = numeroDepartamento;
-	}
-    
-    
-	
+
+    public String getCedulaCondomino() {
+        return cedulaCondomino;
+    }
+
+    public void setCedulaCondomino(String cedulaCondomino) {
+        this.cedulaCondomino = cedulaCondomino;
+    }
+
+    public String getNombreCondomino() {
+        return nombreCondomino;
+    }
+
+    public void setNombreCondomino(String nombreCondomino) {
+        this.nombreCondomino = nombreCondomino;
+    }
+
+    public String getApellidoCondomino() {
+        return apellidoCondomino;
+    }
+
+    public void setApellidoCondomino(String apellidoCondomino) {
+        this.apellidoCondomino = apellidoCondomino;
+    }
+
+    public String getCelularCondomino() {
+        return celularCondomino;
+    }
+
+    public void setCelularCondomino(String celularCondomino) {
+        this.celularCondomino = celularCondomino;
+    }
+
+    public String getTelefonoCondomino() {
+        return telefonoCondomino;
+    }
+
+    public void setTelefonoCondomino(String telefonoCondomino) {
+        this.telefonoCondomino = telefonoCondomino;
+    }
+
+    public String getNumeroBloque() {
+        return numeroBloque;
+    }
+
+    public void setNumeroBloque(String numeroBloque) {
+        this.numeroBloque = numeroBloque;
+    }
+
+    public String getNumeroDepartamento() {
+        return numeroDepartamento;
+    }
+
+    public void setNumeroDepartamento(String numeroDepartamento) {
+        this.numeroDepartamento = numeroDepartamento;
+    }
 }
